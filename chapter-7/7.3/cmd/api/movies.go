8bab38aa94ca4ff87b9d93a4f81325a531b59f85
Santhosh K S <santhoshachar08@gmail.com>
2024-08-22 10:32:10 +0530

    chapter-7.3

---
+++ chapter-7/7.3/cmd/api/movies.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	// "encoding/json"
+	"errors"
+	"fmt"
+	"greelight.techkunstler.com/internal/data"
+	"greelight.techkunstler.com/internal/validator"
+	"net/http"
+	// "time"
+)
+
+// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
+// return a plain-text placeholder response.
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Decrae an anonymus struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are subset
+	//of the move sturct that we created earlier).
+	// This struct will be our *target decode destination*
+
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"` // Make this field a data.Runtime type.
+		Genres  []string     `json:"genres`
+	}
+
+	/* // Initialize a new json.Decoder instance which reads from the request body, and then use the Decode() method to decode the body contents in to the input struct.
+	// Importantly, notice that when we call Decode() we pass a *pointer* to the input struct as the target decode destination. If there was an error during decoding, we
+	// also use our generic errorResponse() helper to send tht client a 400 Bad Request respone
+	// containing the error message.
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	// Dump the contents of the input struct in a HTTP response. */
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		// app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	// Use the Valid() method to see if any of the checks failed. If they did, then use the failedValidationResponse() helper to send a response to the clien,
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Call the Insert() method on our movies model, passing in a pointer to the validated movie struct.
+	// Thiswill create a recor in the database and update the
+	// movie struct with the system-generated information.
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// When sending a HTTP response, we want to include a location header to let the client know which URL they can find the newly created resource at. We make an empty
+	// http.Header map and then use the Set() method to add a new Location header,
+	// interpolating the system-generated ID for our new movie in the URL.
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+	// Write a JSON response with a 201 Created status code, themovie data in the
+	// response body, and the location header.
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+	// passing in the v.Errors map.
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+// Add a showMoivew handler for the "GET /v1/movies/:id" endpoint. For now, we retrive the
+// the interpolated "id" parameter from the current URL and include it in a placeholder response.
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// Call the Get() method to fetch the data for a specific movie. We also need to use
+	// the errors.Is() function to check if it returns a data.ErrRecordNotFound error,
+	// in which case we send a 404 Not Found response to the client.
+
+	movie, err := app.models.Movies.Get(id)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+
+		}
+		return
+	}
+
+	/* // Create a new instance of the Movie struct, containing the ID we extracted from
+	// the URL and some dummy data. Also notice that we deliberately haven't set a
+	// value for the Year field
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	} */
+
+	// Encode the struct to JSON and send it as the HTTP response.
+	// Create an envelope {"movie": movie} instance and pass it to writeJSON(), instead of
+	// passing the plain movie struct.
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		/* app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError) */
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
